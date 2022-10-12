f462c0685392913b7d31a13a21bd708acf01b9b3
Kaan Keleş <safa-kaan-safa@hotmail.com>
2022-10-13 01:49:02 +0300

    youtube left menü neredeyse bitti temelleri atıldı

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,17 @@ module.exports = {
       borderCl: "#303030",
       placeHolderCl: "#888888",
       buttonBg: "#323232",
+      textcolor: "#d1d1d1",
+      pickTextColor: "#FBFAFA",
+      navHoverBg: "#383838",
+      fadeTextCl: "#AAAAAA",
     },
     extend: {
       width: {
         128: "550px",
+      },
+      height: {
+        400: "3600px",
       },
     },
   },
